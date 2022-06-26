abe88d16b6dbe106f335c735309a1afd91ab0e38
jae <mjj0404@gmail.com>
2022-06-25 22:41:30 -0400

    added auth middleware

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,11 +13,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
 {
     use Authenticatable, Authorizable, HasFactory;
 
-    protected $fillable = [
-        'email'
-    ];
     protected $primaryKey = 'userid';
-
     public $timestamps = false;
 
 }
